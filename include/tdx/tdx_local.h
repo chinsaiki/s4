019aeb39a4f82c1fcca09d1bf1eb7d085290a926
chinsaiki <chinsaiki@outlook.com>
2021-06-01 20:32:24 +0800

    修正Linux下路径错误。

--- include/tdx/tdx_local.h
+++ include/tdx/tdx_local.h
@@ -55,11 +55,11 @@ private:
 
 	std::string m_tdxPath;
 
-	const std::string m_minuFolderSZ = "\\vipdoc\\sz\\minline\\";
-	const std::string m_minuFolderSH = "\\vipdoc\\sh\\minline\\";
+	const std::string m_minuFolderSZ = "/vipdoc/sz/minline/";
+	const std::string m_minuFolderSH = "/vipdoc/sh/minline/";
 
-	const std::string m_dayFolderSZ = "\\vipdoc\\sz\\lday\\";
-	const std::string m_dayFolderSH = "\\vipdoc\\sh\\lday\\";
+	const std::string m_dayFolderSZ = "/vipdoc/sz/lday/";
+	const std::string m_dayFolderSH = "/vipdoc/sh/lday/";
 
 private:
 	static int parseTime(const struct tdxRawMinuK_t& rawData, struct minuK_t& minuK);
